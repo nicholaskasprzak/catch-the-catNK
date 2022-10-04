4fdf9436f50c4151e316c17dc3372e7cd5acafc5
Kasprzak <nicholas.kasprzak@mymail.champlain.edu>
2022-10-04 14:11:50 -0400

    Adding slight progress on pathfinding implementation

--- examples/catchthecat/Agent.h
+++ examples/catchthecat/Agent.h
@@ -3,6 +3,22 @@
 #include "Point2D.h"
 #include <map>
 
+struct queueEntry
+{
+	queueEntry() = default;
+	//queueEntry(Point2D pos, int weight) { this->pos = pos; this->weight = weight; }
+	
+	Point2D pos;
+	int weight; // accumulated
+	// bool visited?
+
+	bool operator< (queueEntry& rhs)
+	{
+		return weight < rhs.weight;
+	}
+
+};
+
 class World;
 
 class Agent {
@@ -10,10 +26,11 @@ public:
   explicit Agent()= default;;
   virtual Point2D Move(World*)=0;
 
-  std::map<int, std::map<int, bool>> getVisited() { return visited; }
+  //std::map<int, std::map<int, bool>> getVisited() { return visited; }
 
 private:
-  std::map<int, std::map<int, bool>> visited;
+  // This needs to be built to represent all available spaces.
+  std::map<int, std::map<int, queueEntry>> visited;
 };
 
 #endif  // AGENT_H
--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -3,23 +3,37 @@
 #include <stdexcept>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 // Goal of the cat is to find the shortest path to the edge of the screen
 Point2D Cat::Move(World* world) {
   Point2D catPos = world->getCat();
+  std::priority_queue<Point2D> frontier; // sort by closest to goal?
 
   // Bootstrap first element
-  
+  frontier.push(catPos);
+
   // While all elements haven't been visited, visit them
+  while (!frontier.empty())
+  {
+	  // Get first element from queue
+	  Point2D current = frontier.top();
+	  //frontier.pop(); // Should this be removed?
 
-	// Get first element from queue
+	  // Mark the head as visited
+	  
 
-	// Mark the head as visited
-	
-	// For each neighbor:
-	// - Check if it hasnt been visited
-	// - Check if it is not in the queue
-	// - Check if it can be visited
-	// - Add to the queue adding 1 to the weight
-	// - Mark the neighbor as it came from the current head as popped
+	  // For each neighbor:
+	  // - Check if it hasnt been visited
+	  // - Check if it is not in the queue
+	  // - Check if it can be visited
+	  // - Add to the queue adding 1 to the weight
+	  // - Mark the neighbor as it came from the current head as popped
+  }
+
+  // Then create a lowest weight path to the exit from what was visited
+
+  // First element is the cat and the last element is the catcher
+
+  // Would an optimization be to only reconstrust the path if it gets blocked?
 }
